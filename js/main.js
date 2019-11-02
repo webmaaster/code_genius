478eb1463e1ae5ff0aa31d4d1223c7957376662c
webmaaster <wecodewetest@gmail.com>
2019-11-02 19:13:48 -0400

    All files added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Initialize and add the map
+function initMap(){
+
+    //Your location
+    const loc={lat: 42.361145, lng: -71.057083};
+    //Centered map on location
+    const map = new google.maps.Map(doncument.querySelector('.map'), {
+        zoom: 14,
+        center:loc
+    });
+    //The marker, positioned at location
+    const marker = new google.maps.Marker({position: loc, map});
+}
